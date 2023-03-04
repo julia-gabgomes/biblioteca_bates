e83ab48197aad6fa48d807f417ca1b5acd60a28b
douglasB834 <douglas_borges09@hotmail.com>
2023-03-04 08:19:37 -0300

    feat: create model User and first migrate

--- users/models.py
+++ users/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    ...
+    email = models.EmailField(max_length=127)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthdate = models.DateField(null=True)
+    is_blocked = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
+    
