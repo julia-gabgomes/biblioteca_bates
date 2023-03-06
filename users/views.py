d0c95a1b913c50c952a25d8977297dbd97c61678
Henrique Carderan <rike.c.12@gmail.com>
2023-03-06 15:16:53 -0300

    Feat: add UserViews, urls, serialziers and permissions

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import get_object_or_404
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.generics import (ListCreateAPIView)
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from .permissions import IsAccountOwner
+from rest_framework import generics
+
 # Create your views here. Obrigatorio usar Generic views
 
 
-class UserView(ListCreateAPIView):
+class UserView(generics.ListCreateAPIView):
+
     queryset = User.objects.all()
-    ...
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
