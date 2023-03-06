9909ed345f0b51a964ed7af27577d2a56de54f04
Douglas S Borges <86388680+DouglasB834@users.noreply.github.com>
2023-03-06 10:00:01 -0300

    Update models.py

--- loans/models.py
+++ loans/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Loan(models.Model):
     loan_date = models.DateTimeField(auto_now_add=True)
     return_date = models.DateTimeField()
+    is_delayed = models.BooleanField(default=False , blank=False)
 
     copy_id = models.ForeignKey(
         "copies.Copy", on_delete=models.RESTRICT, related_name="loans"
