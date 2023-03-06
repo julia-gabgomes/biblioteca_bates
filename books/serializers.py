ed9ca6f626273c3cac62bb7faef36db5830c9f0e
douglasB834 <douglas_borges09@hotmail.com>
2023-03-06 15:14:16 -0300

    feat: creat serializers, permission and view

--- books/serializers.py
+++ books/serializers.py
@@ -1,1 +1,30 @@
-# from rest_framework import serializers
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = [
+            "id",
+            "isbn",
+            "title",
+            "author",
+            "publisher",
+            "edition",
+            "genre",
+            "language",
+            "pages_number",
+        ]
+        extra_kwargs = {
+            "isbn": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=Book.objects.all(),
+                        message="isbn already exists, must be unique,",
+                    )
+                ]
+            },
+        }
