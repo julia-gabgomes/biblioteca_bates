03d87cb402753cd38529d8af8dc989d11aa2127b
douglasB834 <douglas_borges09@hotmail.com>
2023-03-09 17:35:07 -0300

    refactor returno books

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+
+from copies.models import Copy
 from .models import Loan
+from users.serializers import UserSerializer
 
 
 class LoanSerializer(serializers.ModelSerializer):
-    # title_book = serializers.SerializerMethodField()
+    title_book = serializers.CharField(source="copy.book.title")
+    # user = UserSerializer()
 
     class Meta:
         model = Loan
@@ -18,18 +24,22 @@ class LoanSerializer(serializers.ModelSerializer):
             "isbn",
             "copy",
             "user",
+            "title_book",
         ]
-        # extra_kwargs = {
-        #     "isbn": {
-        #         "validators": [
-        #             UniqueValidator(Loan.objects.all(), "This field must be unique.")
-        #         ],
-        #         "required": True,
-        #     },
-        # }
+        depth = 2
 
     def create(self, validated_data):
         return Loan.objects.create(**validated_data)
 
-    # def get_title_book(self, obj):
-    #     return obj.copy.title_book
+    def update(self, instance, validated_data):
+        copy = get_object_or_404(Copy, id=instance.copy_id)
+
+
+        instance.returned = datetime.today()
+        copy.is_loaned = False
+        instance.is_active = False
+        instance.is_delayed = False
+        instance.save()
+        copy.save()
+
+        return instance
--- loans/views.py
+++ loans/views.py
@@ -36,11 +36,11 @@ class LoanView(generics.ListCreateAPIView):
         if user.is_blocked:
             return Response({"message": "User has books to return"}, 401)
 
-        loan = Loan.objects.filter(isbn=isbn)
-        if loan:
-            return Response(
-                {"message": "This user already have a copy with this isbn"}, 401
-            )
+        # loan = Loan.objects.filter(isbn=isbn)
+        # if loan:
+        #     return Response(
+        #         {"message": "This user already have a copy with this isbn"}, 401
+        #     )
 
         book = get_object_or_404(Book, isbn=isbn)
         if book.count_loaned_copies:
@@ -87,16 +87,20 @@ class LoanReturnView(generics.UpdateAPIView):
     lookup_url_kwarg = "user_id"
     serializer_class = LoanSerializer
 
-    def perform_update(self, serializer):
-        isbn = self.request.data["isbn"]
-        user = get_object_or_404(User, id=self.kwargs.get("user_id"))
-        loan = get_object_or_404(Loan, isbn=isbn)
+    # def perform_update(self, serializer):
+    # isbn = self.request.data["isbn"]
+    # id_copy = serializer.data["copy"]
+    # user = get_object_or_404(User, id=self.kwargs.get("user_id"))
+    # loan = get_object_or_404(Loan, isbn=isbn)
+    # copy = get_object_or_404(Copy, id=id_copy)
 
-        loan.returned = datetime.today()
-        loan.is_active = False
-        loan.copy.is_loaned = False
-        loan.is_delayed = False
-        loan.save()
-        # serializer = LoanSerializer(loan)
+    # loan.returned = datetime.today()
+    # loan.is_active = False
+    # copy.is_loaned = False
+    # loan.is_delayed = False
 
-        # return Response(status=201)
+    # copy.save()
+    # loan.save()
+    # serializer = LoanSerializer(loan)
+
+    # return Response(status=201)
